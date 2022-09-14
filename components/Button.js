e0b4e04ffb2cda204c05cbf70684735e64435863
Ben Dalton <ben.dalton@roofstock.com>
2022-09-14 12:21:22 -0400

    different button

--- components/Button.js
+++ components/Button.js
@@ -1,5 +1,11 @@
-const Button = () => (
-  <button onClick={() => alert('ouch!')}>This button is from far away</button>
-);
+import React from 'react';
+
+const style = {
+  background: '#ffc600',
+  color: '#000',
+  padding: 12,
+};
+
+const Button = () => <button style={style}>Next JS Button</button>;
 
 export default Button;
